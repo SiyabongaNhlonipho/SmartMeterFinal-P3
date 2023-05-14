c7d6a5939eff43212be075c675a7449b4d7a9938
SiyabongaNhlonipho <220039943@stu.ukzn.ac.za>
2023-05-14 16:35:33 +0200

    back online

--- client/src/Pages/dashboard/reset-password.js
+++ client/src/Pages/dashboard/reset-password.js
@@ -1,50 +1,30 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useRef } from 'react'
 import { useState } from 'react'
 import { useAppContext } from '../../Context/appContext'
-import emailjs from '@emailjs/browser';
 
 const ResetPassword = () => {
-  const {email,code,expireIn,sendEmail,Allnames,AlllastNames,Allemails,Alllocations,getData} = useAppContext()
+  const {email,code,expireIn,sendEmail} = useAppContext()
   const[emailEntered,setEmailEntered] =useState('')
   const[capture_emailEntered,setCapture_emailEntered] =useState('')
-  const emailRef = useRef()
-  //const form = useRef();
-
-
-  useEffect(()=>{
-             getData()
-            getAllUsers ()
-   }, [])
-    
+  
+       const emailRef = useRef()
 
 const sendOTP = (event) => { 
     event.preventDefault()
     setCapture_emailEntered(`${emailEntered}`)
-   // let otpData;
-    /*for (let i =0 ; i< Allemails.length; i++){
-      
-      if(Allemails[i]===emailEntered){
-
-           otpData ={email:capture_emailEntered,code:code,expireIn:expireIn}
-       emailjs.sendForm("service_55u9ony","template_xfbz84h", form.current,"UtuJZstWyBB22fTow")
-          alert("Message sent Successfully!!");
-      }
-    }*/
-   let otpData ={email:capture_emailEntered,code:code,expireIn:expireIn}
-       emailjs.sendForm("service_55u9ony","template_xfbz84h", emailRef.current,"UtuJZstWyBB22fTow")
-          alert("Message sent Successfully!!");
-       //calling function
-      sendEmail(otpData )
+  let otpData ={email:capture_emailEntered,code:code,expireIn:expireIn}
+  //calling function
+  sendEmail(otpData )
 }
 
   return (
-     <form onSubmit={sendOTP} >
+      <form onSubmit={sendOTP}>
               <h3> Reset Password </h3>
               <div>
-              <label>Email </label>
+              <label>Email {capture_emailEntered}</label>
               </div>
               <div>
-              <input type="email" name="user_email" size="50" ref={emailRef} onChange = {(event) => setEmailEntered(event.target.value)}/>
+              <input type="email" name="email" size="50" ref={emailRef} onChange = {(event) => setEmailEntered(event.target.value)}/>
               </div>
               <p> 
                 <button type="button" className="member-btn" onClick={sendOTP}>Send</button>
@@ -52,7 +32,6 @@ const sendOTP = (event) => {
                  <a href="https://smart-meter-g10-final.onrender.com/login" class="button">Back</a>
               </p>
       </form>
-     
   )
 }
 
