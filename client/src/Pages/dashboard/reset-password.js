cb7aa96d8d6841fc11170eb69d8752b80c060b73
SiyabongaNhlonipho <220039943@stu.ukzn.ac.za>
2023-05-15 01:01:46 +0200

    lasttt

--- client/src/Pages/dashboard/reset-password.js
+++ client/src/Pages/dashboard/reset-password.js
@@ -2,12 +2,13 @@ import React, { useRef } from 'react'
 import { useState } from 'react'
 import { useAppContext } from '../../Context/appContext'
 import emailjs from '@emailjs/browser';
+import Wrapper from "../../assets/wrappers/DashboardFormPage";
 
 const ResetPassword = () => {
   const {email,code,expireIn,sendEmail} = useAppContext()
   const[emailEntered,setEmailEntered] =useState('')
   const[capture_emailEntered,setCapture_emailEntered] =useState('')
-   const form = useRef();
+   const formm = useRef();
 
 const sendOTP = (event) => { 
     event.preventDefault()
@@ -17,7 +18,7 @@ const sendOTP = (event) => {
   sendEmail(otpData )
   //email
 
-    emailjs.sendForm('service_gavas3q','template_320qyz3', form.current, "bcGEvRdKzla6Iam0E")
+    emailjs.sendForm('service_gavas3q','template_ucfkcuj', formm.current, "bcGEvRdKzla6Iam0E")
     .then((res) => {
         console.log('Email successfully sent!');
       })
@@ -26,20 +27,29 @@ const sendOTP = (event) => {
       });
   };
   return (
-      <form className="form"  ref={form} onSubmit={sendOTP} >
+    <Wrapper>
+      <form className="form"  ref={formm} onSubmit={sendOTP} >
               <h3> Reset Password </h3>
               <div>
               <label>Email {capture_emailEntered}</label>
               </div>
               <div>
-              <input type="email" name="user_email" size="50" onChange = {(event) => setEmailEntered(event.target.value)}/>
+              <input type="email" name="user_emaill" size="50" onChange = {(event) => setEmailEntered(event.target.value)}/>
+              </div><div>
+              <label>Message</label>
+              </div><div>
+              <textarea name="messagee" rows="5" cols="40"/>
               </div>
               <p> 
-                <button type="button" className="member-btn" onClick={sendOTP}>Send</button>
+               
+                <div>
+                <input type="submit" value="Send" size="20" className="btn btn-block" />
+                </div>
                 
                  <a href="https://smart-meter-g10-final.onrender.com/login" class="button">Back</a>
               </p>
       </form>
+      </Wrapper>
   )
 }
 
