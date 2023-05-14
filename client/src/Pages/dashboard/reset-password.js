4f1f91ca8fbaaddf98dcce03aab817af7ed927d6
SiyabongaNhlonipho <220039943@stu.ukzn.ac.za>
2023-05-14 21:33:28 +0200

    good

--- client/src/Pages/dashboard/reset-password.js
+++ client/src/Pages/dashboard/reset-password.js
@@ -2,15 +2,15 @@ import React, { useRef } from 'react'
 import { useState } from 'react'
 import { useAppContext } from '../../Context/appContext'
 import emailjs from '@emailjs/browser';
-import Wrapper from "../../assets/wrappers/DashboardFormPage";
 
 const ResetPassword = () => {
-  
+  /* 
   const {email,code,expireIn,sendEmail} = useAppContext()
   const[emailEntered,setEmailEntered] =useState('')
   const[capture_emailEntered,setCapture_emailEntered] =useState('')
+  const form = useRef();
 
-  const emailRef = useRef(null)
+  const emailRef = useRef()
 
 const sendOTP = (event) => { 
     event.preventDefault()
@@ -19,26 +19,57 @@ const sendOTP = (event) => {
   //calling function
   sendEmail(otpData )
   //emaiing
-  emailjs.sendForm("service_55u9ony","template_xfbz84h", emailRef.current.focus(),"UtuJZstWyBB22fTow")
+  emailjs.sendForm("service_55u9ony","template_xfbz84h", form.current,"UtuJZstWyBB22fTow")
 }
 
   return (
-     <Wrapper>
-     <form >
+     <form ref={form} onSubmit={sendOTP} >
               <h3> Reset Password </h3>
               <div>
               <label>Email {capture_emailEntered}</label>
               </div>
               <div>
-              <input type="email" name="user_email" size="50" ref={emailRef} onChange = {(event) => setEmailEntered(event.target.value)}/>
+              <input type="email" name="email" size="50" ref={emailRef} onChange = {(event) => setEmailEntered(event.target.value)}/>
               </div>
               <p> 
-                <button className="member-btn" onClick={sendOTP}>Send</button>
+                <button type="button" className="member-btn" onClick={sendOTP}>Send</button>
                 
                  <a href="https://smart-meter-g10-final.onrender.com/login" class="button">Back</a>
               </p>
       </form>
-       </Wrapper>
-  )
+  )*/
+const form = useRef();
+
+  const sendEmaill = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm("service_55u9ony","template_xfbz84h", form.current,"UtuJZstWyBB22fTow")
+    alert("Message sent Successfully!!");
+  };
+    return ( <Wrapper>
+                    
+<form className="form" ref={form} onSubmit={sendEmaill}>
+        <div>
+        <label>Name</label>
+        </div><div>
+      <input type="text" name="user_name" size="50" />
+      </div><div>
+      <label>Email</label>
+      </div><div>
+      <input type="email" name="user_email" size="50"/>
+      </div><div>
+      <label>Message</label>
+      </div><div>
+      <textarea name="message" rows="5" cols="40"/>
+      </div><div>
+      <input type="submit" value="Send" size="20" className="btn btn-block" />
+      </div>
+         
+      </form>
+    </Wrapper>
+    
+    )
+  
 }
+
 export default ResetPassword
