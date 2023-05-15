8d3f1a7d4ef09d21143c8606d3535e9080de5860
SiyabongaNhlonipho <220039943@stu.ukzn.ac.za>
2023-05-15 02:27:47 +0200

    let's try it

--- client/src/Pages/dashboard/reset-password.js
+++ client/src/Pages/dashboard/reset-password.js
@@ -1,8 +1,11 @@
 import React, { useRef } from 'react'
 import { useState } from 'react'
 import { useAppContext } from '../../Context/appContext'
+import emailjs from '@emailjs/browser';
+import Wrapper from "../../assets/wrappers/DashboardFormPage";
 
 const ResetPassword = () => {
+  /*
   const {email,code,expireIn,sendEmail} = useAppContext()
   const[emailEntered,setEmailEntered] =useState('')
   const[capture_emailEntered,setCapture_emailEntered] =useState('')
@@ -30,7 +33,39 @@ const sendOTP = (event) => {
                  <a href="https://smart-meter-g10-final.onrender.com/login" class="button">Back</a>
               </p>
       </form>
-  )
+  )*/
+ const sec_form = useRef();
+
+  const sendE = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm("service_jaq4qlp","template_ltc29pd", sec_form.current,"bcGEvRdKzla6Iam0E")
+    alert("Message sent Successfully!!");
+  };
+    return ( <Wrapper>
+                    
+<form className="form" ref={sec_form} onSubmit={sendE}>
+        <div>
+        <label>Name</label>
+        </div><div>
+      <input type="text" name="name" size="50" />
+      </div><div>
+      <label>Email</label>
+      </div><div>
+      <input type="email" name="email" size="50"/>
+      </div><div>
+      <label>Message</label>
+      </div><div>
+      <textarea name="user_message" rows="5" cols="40"/>
+      </div><div>
+      <input type="submit" value="Send" size="20" className="btn btn-block" />
+      </div>
+         
+      </form>
+    </Wrapper>
+    
+    )
+
 }
 
 export default ResetPassword
