399900f8e3750894482b0563c3856385f07f7dba
SiyabongaNhlonipho <220039943@stu.ukzn.ac.za>
2023-05-25 00:05:09 +0200

    Finall

--- client/src/Pages/dashboard/recharge.js
+++ client/src/Pages/dashboard/recharge.js
@@ -46,7 +46,7 @@ const Recharge = () => {
                  alert(" Successfully  Recharged  !!");
            }
 
-          if (capture_Water === voucher[i] && capture_electricity === voucher[i] ){
+          /*if (capture_Water === voucher[i] && capture_electricity === voucher[i] ){
 
                 for (let j =0 ; j< outstanding_Balance_in_Rands.length; j++){
                 x= parseFloat(Value[i])
@@ -56,7 +56,7 @@ const Recharge = () => {
 
                       alert(" Successfully  Recharged For Water Meter !!");
                        alert(" You can Not Use The same Voucher Twice!!");
-           }
+           }*/
          
       } //end 1st for loop
       PostVoucher(voucherr) 
